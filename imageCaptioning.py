08c61060e37bb0ef316f5aae810a38e9769897b9
NeilSharma425 <neilsharma425@gmail.com>
2025-05-04 02:18:37 -0700

    create base captioning functions

--- imageCaptioning.py
+++ imageCaptioning.py
@@ -1,17 +1,24 @@
+#import gradio as gr
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
 
 # Initialize the processor and model
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
-
-# Load image
-image = Image.open("space.jpg")
-
-# prep image
-inputs = processor(image, return_tensors="pt")
-
-# gen captions
-outputs = model.generate(**inputs)
-caption = processor.decode(outputs[0],skip_special_tokens=True)
-print("Generated Caption:", caption)
+def generate_caption(image): 
+    # Load image
+    image = Image.open("space.jpg")
+    # prep image
+    inputs = processor(image, return_tensors="pt")
+    # gen captions
+    outputs = model.generate(**inputs)
+    caption = processor.decode(outputs[0],skip_special_tokens=True)
+    return caption
+def caption_img(img):
+    try:
+        caption = generate_caption(img)
+        return caption
+    except Exception as e:
+        return f"Error: {str(e)}"
+    
+caption_img("space.jpg")
